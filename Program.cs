b3489df37926b93d65551f8c31ae91e429a7b9da
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 14:33:45 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anonymousmethod
+{
+    class Program
+    {
+        public delegate void Mydelg(string s);
+
+        static void Main(string[] args)
+        {
+
+            //anonymous method
+            Mydelg delgt = a => Console.WriteLine(a);
+            delgt("hello");
+
+
+        }
+    }
+}
